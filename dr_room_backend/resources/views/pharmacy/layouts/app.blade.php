f17d751ae8ab5da948d1acab7485ea6ef576f332
ibrahim-dev11 <ibrahim.ahmed@pti.edu.krd>
2026-08-22 13:28:04 +0300

    refactor: remove offers navigation link from pharmacy layout

--- dr_room_backend/resources/views/pharmacy/layouts/app.blade.php
+++ dr_room_backend/resources/views/pharmacy/layouts/app.blade.php
@@ -282,11 +282,6 @@
                     <svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"/></svg>
                     دەرمانەکان (کۆگا)
                 </a>
-
-                <a href="{{ route('pharmacy.offers.index') }}" class="nav-item {{ request()->routeIs('pharmacy.offers.*') ? 'active' : '' }}">
-                    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7"/></svg>
-                    ئۆفەرەکان
-                </a>
                 
                 <div class="nav-label">تر</div>
                 
